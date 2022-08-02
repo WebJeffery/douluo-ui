b6a7d3785683af307bfca6c79fc93d1d8ba86606
谢志非 <xiezf@91160.com>
2022-08-02 18:53:41 +0800

    perf: 增加 eslint 配置规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,22 @@
 module.exports = {
+  root: true,
   env: {
     browser: true,
     es2021: true,
+    node: true
   },
   extends: [
     'plugin:vue/essential',
+    'prettier',
+    'plugin:prettier/recommended'
     // 'airbnb-base',
   ],
-  // parser: "vue-eslint-parser",
+  parser: 'vue-eslint-parser',
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module',
     parser: 'babel-eslint'
   },
-  plugins: [
-    'vue',
-  ],
-  rules: {
-  },
+  plugins: ['vue'],
+  rules: {}
 };
