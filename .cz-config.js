854e3ac4254432e25a43e33a3d7ff9cc8810beaf
谢志非 <xiezf@91160.com>
2022-08-03 17:28:50 +0800

    :construction_worker: ci: commit 规范

--- .cz-config.js
+++ .cz-config.js
@@ -1,54 +1,60 @@
 module.exports = {
   types: [
-    { value: 'feat', name: 'feat: 一个新的特性' },
-    { value: 'fix', name: 'fix: 修复一个Bug' },
-    { value: 'docs', name: 'docs: 变更的只有文档' },
-    { value: 'style', name: 'style: 代码风格,格式修复' },
-    { value: 'refactor', name: 'refactor: 代码重构，注意和feat、fix区分开' },
-    { value: 'perf', name: 'perf: 码优化,改善性能' },
-    { value: 'test', name: 'test: 测试' },
-    { alue: 'chore', name: 'chore: 变更构建流程或辅助工具' },
-    { value: 'revert', name: 'revert: 代码回退' },
-    { value: 'init', name: 'init: 项目初始化' },
-    { value: 'ci', name: 'ci：持续集成，自动化流程配置修改' },
-    { value: 'build', name: 'build: 变更项目构建或外部依赖' },
-    { value: 'WIP', name: 'WIP: 进行中的工作' }
+    { value: ':sparkles: feat', name: '✨ feat: 一项新功能' },
+    { value: ':bug: fix', name: '🐛 fix: 修复一个Bug' },
+    { value: ':memo: docs', name: '📝 docs: 文档变更' },
+    { value: ':lipstick: style', name: '💄 style: 代码风格，格式修复' },
+    { value: ':recycle: refactor', name: '♻️ refactor: 代码重构，注意和feat、fix区分开' },
+    { value: ':zap: perf', name: '⚡️ perf: 代码优化,改善性能' },
+    { value: ':white_check_mark: test', name: '✅ test: 测试' },
+    { value: ':rocket: chore', name: '🚀 chore: 变更构建流程或辅助工具' },
+    { value: ':rewind: revert', name: '⏪️ revert: 代码回退' },
+    { value: ':tada: init', name: '🎉 init: 项目初始化' },
+    { value: ':construction_worker: ci', name: '👷 对CI配置文件和脚本的更改' },
+    { value: ':package: build', name: '📦️ build: 变更项目构建或外部依赖' },
+    { value: ':construction: WIP', name: '🚧 WIP: 进行中的工作' }
   ],
-  scopes: [],
-  allowTicketNumber: false,
-  isTicketNumberRequired: false,
-  ticketNumberPrefix: 'TICKET-',
-  ticketNumberRegExp: '\\d{1,5}',
+  scopes: [
+    { name: 'component' },
+    { name: 'config' },
+    { name: 'docs' },
+    { name: 'src' },
+    { name: 'examples' },
+    { name: 'play' }
+  ],
+  // allowTicketNumber: false,
+  // isTicketNumberRequired: false,
+  // ticketNumberPrefix: 'TICKET-',
+  // ticketNumberRegExp: '\\d{1,5}',
   // it needs to match the value for field type. Eg.: 'fix'
-  /*
-  scopeOverrides: {
-    fix: [
-      {name: 'merge'},
-      {name: 'style'},
-      {name: 'e2eTest'},
-      {name: 'unitTest'}
-    ]
-  },
-  */
+  // scopeOverrides: {
+  //   feat: [
+  //     { name: 'element' }
+  //   ],
+  //   fix: [
+  //     { name: 'element' },
+  //     { name: 'style' },
+  //   ]
+  // },
   // override the messages, defaults are as follows
   messages: {
-    type: '选择一种你的提交类型:',
-    scope: '选择一个scope (可选):',
+    type: '请选择提交类型(必填):',
+    scope: '请选择一个scope (可选):',
+    customScope: '请输入文件修改范围(可选):',
     // used if allowCustomScopes is true
-    customScope: 'Denote the SCOPE of this change:',
-    subject: '简短说明(最多40个字):',
-    body: '长说明，使用"|"换行(可选)：\n',
-    breaking: '非兼容性说明 (可选):\n',
-    footer: '关联关闭的issue，例如：#12, #34(可选):\n',
-    confirmCommit: '确定提交?'
+    subject: '请简要描述提交(必填):',
+    body: '请输入详细描述，使用"|"换行(可选):\n',
+    breaking: '列出任务非兼容性说明 (可选):\n',
+    footer: '请输入要关闭的issue，例如：#12, #34(可选):\n',
+    confirmCommit: '确定提交此说明吗？'
   },
   allowCustomScopes: true,
   allowBreakingChanges: ['feat', 'fix'],
-  // skip any questions you want
-  skipQuestions: ['scope', 'body', 'breaking'],
-  // limit subject length
-  subjectLimit: 100
+  // 限制 subject 长度
+  subjectLimit: 72
+  // 跳过问题 skip any questions you want
+  // skipQuestions: ['body', 'footer'],
   // breaklineChar: '|', // It is supported for fields body and footer.
   // footerPrefix : 'ISSUES CLOSED:'
   // askForBreakingChangeFirst : true, // default is false
-};
+}
