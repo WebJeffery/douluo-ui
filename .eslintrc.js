53d519e0c79020fc3d8dcf20377957f0d7a73758
谢志非 <xiezf@91160.com>
2022-08-03 11:27:43 +0800

    feat(component): eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    es2021: true,
+    es6: true,
     node: true
   },
   extends: [
@@ -40,4 +40,4 @@ module.exports = {
       }
     ]
   }
-};
+}
